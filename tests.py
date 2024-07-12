00d7959eeabd5e5384213836c6dfc76a344ed715
sgwinter <seangcalgary@gmail.com>
2024-07-12 14:00:21 -0600

    added additional large size tests

--- tests.py
+++ tests.py
@@ -16,22 +16,35 @@ class Tests(unittest.TestCase):
                 num_rows,
         )
 
-    def test_maze_create_cells2(self):
-        num_cols2 = 100
-        num_rows2 = 80
+    def test_maze_create_cells_full_screen(self):
+        num_cols = 100
+        num_rows = 80
         margin = 50
         screen_x = 3440
         screen_y = 1440
-        cell_size_x = (screen_x - 2 * margin) / num_cols2
-        cell_size_y = (screen_y - 2 * margin) / num_rows2
-        m2 = Maze(margin, margin, num_rows2, num_cols2, cell_size_x, cell_size_y)
+        cell_size_x = (screen_x - 2 * margin) / num_cols
+        cell_size_y = (screen_y - 2 * margin) / num_rows
+        m2 = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y)
         self.assertEqual(
                 len(m2._cells),
-                num_cols2,
+                num_cols,
         )
         self.assertEqual(
                 len(m2._cells[0]),
-                num_rows2,
+                num_rows,
+        )
+
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m3 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+                len(m3._cells),
+                num_cols,
+        )
+        self.assertEqual(
+                len(m3._cells[0]),
+                num_rows,
         )
 
 if __name__ == "__main__":
